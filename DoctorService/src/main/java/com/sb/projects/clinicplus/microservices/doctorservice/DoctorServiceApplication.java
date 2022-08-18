aa34ff8aad5fe2872cded9c812adbf2697b30bf3
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-08-18 10:21:17 +0530

    Registering all services with Eureka Server Service discovery. Next I will start communication between different microservices

--- DoctorService/src/main/java/com/sb/projects/clinicplus/microservices/doctorservice/DoctorServiceApplication.java
+++ DoctorService/src/main/java/com/sb/projects/clinicplus/microservices/doctorservice/DoctorServiceApplication.java
@@ -2,8 +2,10 @@ package com.sb.projects.clinicplus.microservices.doctorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
--- PatientService/src/main/java/com/sb/projects/clinicplus/microservices/patientservice/PatientServiceApplication.java
+++ PatientService/src/main/java/com/sb/projects/clinicplus/microservices/patientservice/PatientServiceApplication.java
@@ -2,8 +2,10 @@ package com.sb.projects.clinicplus.microservices.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PatientServiceApplication {
 
     public static void main(String[] args) {
