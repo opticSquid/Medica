00bbbbc448111ffc4e6622fbed4aea3e61a0930e
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-10-21 12:19:27 +0530

    Starting to configure project stricture to implement api gateway and load balancing using spring cloud gateway

---
+++ Gateway/src/test/java/com/sb/projects/clinicplus/microservices/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.projects.clinicplus.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
